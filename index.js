f80b94e9f9bbd845cf6594541428816ee8716247
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-09-17 21:32:25 -0300

    different message

--- index.js
+++ index.js
@@ -59,31 +59,34 @@ app.post('/webhook', (req, res) => {
                     messaging_product: "whatsapp",
                     recipient_type: "individual",
                     to: from,
-                    type: "flow",
-                    header: {
-                        type: "text",
-                        text: "Flow message header"
-                    },
-                    body: {
-                        text: "Flow message body"
-                    },
-                    footer: {
-                        text: "Flow message footer"
-                    },
-                    action: {
-                        name: "flow",
-                        parametes: {
-                            flow_message_version: "3",
-                            flow_token: "AQAAAAACS5FpgQ_cAAAAAD0QI3s.",
-                            flow_id: "1",
-                            flow_cta: "Book!",
-                            flow_action: "navigate",
-                            flow_action_payload: {
-                                screen: "<SCREEN_NAME>",
-                                data: {
-                                    product_name: "name",
-                                    product_description: "description",
-                                    product_price: 100
+                    type: "interactive",
+                    interactive: {
+                        type: "flow",
+                        header: {
+                            type: "text",
+                            text: "Flow message header"
+                        },
+                        body: {
+                            text: "Flow message body"
+                        },
+                        footer: {
+                            text: "Flow message footer"
+                        },
+                        action: {
+                            name: "flow",
+                            parametes: {
+                                flow_message_version: "3",
+                                flow_token: "AQAAAAACS5FpgQ_cAAAAAD0QI3s.",
+                                flow_id: "1",
+                                flow_cta: "Book!",
+                                flow_action: "navigate",
+                                flow_action_payload: {
+                                    screen: "<SCREEN_NAME>",
+                                    data: {
+                                        product_name: "name",
+                                        product_description: "description",
+                                        product_price: 100
+                                    }
                                 }
                             }
                         }
