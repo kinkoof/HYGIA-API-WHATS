6d988969b1a06ee239b092c917b1ef33a0915932
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-09-22 16:12:02 -0300

    teste 22/09

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-    VERIFY_TOKEN: process.env.VERIFY_TOKEN,
-    PORT: process.env.PORT || 3000,
-};
--- middlewares/verifyWebhook.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    const token = req.query['hub.verify_token'];
-    if (token === process.env.VERIFY_TOKEN) {
-        next();
-    } else {
-        res.status(403).send('Forbidden');
-    }
-};
