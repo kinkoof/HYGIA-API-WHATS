09e275f7fe9d15f04880db3280f801057a842875
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-09-22 14:26:01 -0300

    add email in registration

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -2,44 +2,55 @@ const axios = require('axios');
 const { ACCESS_TOKEN } = require('../config/config');
 const userFlows = require('../state/userFlows');
 
-// Função para enviar mensagem de endereço
-const sendAddressMessage = (phone_number_id, from, res) => {
-    axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/v15.0/${phone_number_id}/messages`,
-        headers: {
-            'Authorization': `Bearer ${ACCESS_TOKEN}`,
-            'Content-Type': 'application/json'
-        },
-        data: {
-            messaging_product: 'whatsapp',
-            recipient_type: 'individual',
-            to: from,
+// Função reutilizável para enviar mensagens
+const sendWhatsAppMessage = (phone_number_id, to, text, res, buttons = null, location = null) => {
+    let messageData = {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to,
+    };
+
+    if (buttons) {
+        messageData = {
+            ...messageData,
             type: 'interactive',
             interactive: {
-                type: 'address_message',
-                body: {
-                    text: 'Obrigado pelo seu pedido! Diga-nos para qual endereço gostaria de enviá-lo.'
-                },
-                action: {
-                    name: 'address_message',
-                    parameters: {
-                        country: 'BR' // Código ISO do país
-                    }
-                }
+                type: 'button',
+                header: { type: 'text', text: 'Bem Vindo' },
+                body: { text },
+                action: { buttons: buttons.map(button => ({ type: 'reply', reply: button })) }
             }
-        }
-    })
-    .then(() => res.sendStatus(200))
-    .catch(error => {
-        console.error('Error sending address message:', error);
-        res.sendStatus(500);
-    });
+        };
+    } else if (location) {
+        messageData = {
+            ...messageData,
+            type: 'location',
+            location: {
+                latitude: location.latitude,
+                longitude: location.longitude,
+                name: location.name,
+                address: location.address
+            }
+        };
+    } else {
+        messageData = {
+            ...messageData,
+            type: 'text',
+            text: { body: text }
+        };
+    }
+
+    axios.post(`https://graph.facebook.com/v19.0/${phone_number_id}/messages?access_token=${ACCESS_TOKEN}`, messageData)
+        .then(() => res.sendStatus(200))
+        .catch(error => {
+            console.error('Error sending message:', error);
+            res.sendStatus(500);
+        });
 };
 
-// Função para iniciar o fluxo de registro (pedindo senha)
+// Inicia o fluxo de registro
 const startRegisterFlow = (phone_number_id, from, res) => {
-    userFlows[from] = { step: 'password', data: { phoneNumber: from } };
+    userFlows[from] = { step: 'password', data: { phoneNumber: from } }; // Armazena o número do usuário
     sendWhatsAppMessage(phone_number_id, from, 'Para começar seu registro, defina uma Senha:', res);
 };
 
@@ -58,24 +69,31 @@ const handleRegistrationStep = (phone_number_id, from, userText, res) => {
 
         case 'confirmPassword':
             if (userText === userFlows[from].data.password) {
-                userFlows[from].step = 'email';
+                userFlows[from].step = 'email'; // Avança para o passo do e-mail
                 sendWhatsAppMessage(phone_number_id, from, 'Agora, por favor, informe seu e-mail:', res);
             } else {
                 userFlows[from].step = 'password';
-                sendWhatsAppMessage(phone_number_id, from, 'As senhas não coincidem. Tente novamente.', res);
+                sendWhatsAppMessage(phone_number_id, from, 'As senhas não coincidem. Vamos começar de novo. Por favor defina a', res);
             }
             break;
 
         case 'email':
-            userFlows[from].data.email = userText;
-            userFlows[from].step = 'address';
-            sendAddressMessage(phone_number_id, from, res); // Envia a mensagem pedindo o endereço
+            userFlows[from].data.email = userText; // Armazena o e-mail do usuário
+            userFlows[from].step = 'location'; // Avança para o passo de localização
+            sendWhatsAppMessage(phone_number_id, from, 'Por favor, compartilhe sua localização:', res);
             break;
 
-        case 'address':
-            userFlows[from].data.address = userText; // Armazena o endereço fornecido pelo usuário
-            const { phoneNumber, password, email, address } = userFlows[from].data;
-            saveUserToDatabase(from, { phoneNumber, password, email, address });
+        case 'location':
+            // Aqui você pode salvar a localização enviada (neste exemplo, estamos simulando a localização)
+            const location = {
+                latitude: '-23.550520', // Exemplo de latitude (São Paulo)
+                longitude: '-46.633308', // Exemplo de longitude
+                name: 'São Paulo',
+                address: 'São Paulo, SP, Brasil'
+            };
+            userFlows[from].data.location = location; // Salva a localização do usuário
+            const { phoneNumber, password, email } = userFlows[from].data; // Pega os dados
+            saveUserToDatabase(from, { phoneNumber, password, email, location });
             sendWhatsAppMessage(phone_number_id, from, `Parabéns! Seu registro foi concluído com sucesso.`, res);
             delete userFlows[from];
             break;
@@ -85,7 +103,7 @@ const handleRegistrationStep = (phone_number_id, from, userText, res) => {
 // Função para salvar o usuário no banco de dados
 const saveUserToDatabase = (from, userData) => {
     console.log('Salvando no banco de dados:', { from, ...userData });
-    // Aqui você incluiria a lógica para salvar no banco de dados
+    // Aqui você incluiria a lógica para salvar no banco, como uma inserção no MongoDB, MySQL, etc.
 };
 
 module.exports = { sendWhatsAppMessage, startRegisterFlow, handleRegistrationStep, saveUserToDatabase };
