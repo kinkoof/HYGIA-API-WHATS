b173c7e6ffe2fba8d34744fe74a299c0a4b0f606
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-11-13 04:18:02 -0300

    dfgdf

--- src/controllers/denyOrderController.js
+++ src/controllers/denyOrderController.js
@@ -1,30 +1,35 @@
 const db = require('../config/db');
 
-exports.denyOrder = async (phone_number_id, pharmacyId, orderId, res) => {
+exports.denyOrder = async (req, res) => {
+    const { orderId, pharmacyId } = req.body;
+
     // Verifique se os parâmetros necessários foram passados
     if (!orderId || !pharmacyId) {
         return res.status(400).send('Faltam parâmetros necessários: orderId ou pharmacyId.');
     }
 
     try {
-        // Atualizar o status da ordem para "r" (recusado pela farmácia)
+        // Atualizar o status da ordem para "a" (aceito pela farmácia)
         const [result] = await db.execute(
             `UPDATE orders
              SET status = 'x', pharmacy_id = ?
              WHERE id = ?`,
-            [pharmacyId, orderId]
+            [pharmacyId, orderId]  // Remover a parte `AND pharmacy_id = ?` para teste
         );
 
+
         if (result.affectedRows === 0) {
-            console.log(`Erro: Nenhuma ordem encontrada ou a farmácia não pode recusar o pedido ${orderId}.`);
-            return res.status(400).json({ success: false, message: 'Erro ao recusar o pedido.' });
+            console.log(`Erro: Nenhuma ordem encontrada ou a farmácia não pode rejeitar o pedido ${orderId}.`);
+            return res.status(400).json({ success: false, message: 'Erro ao rejeitar o pedido.' });
         }
 
         // Responder com sucesso
-        return res.status(200).json({ success: true, message: 'Pedido recusado.' });
+        return res.status(200).json({ success: true, message: 'Pedido rejeitado.' });
 
-    } catch (error) {
-        console.error('Erro ao recusar pedido:', error);
+    }catch (error) {
+        console.error('Erro ao rejeitar pedido:', error.message);
+        console.error(error.stack);
         return res.status(500).send('Erro ao processar a solicitação.');
     }
+
 };
