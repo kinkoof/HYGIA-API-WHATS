dc8e51ef2307ca7e0b100965fd991c062c11bcf9
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-11-13 03:45:39 -0300

    asdasd

--- src/controllers/acceptOrderController.js
+++ src/controllers/acceptOrderController.js
@@ -1,6 +1,5 @@
 // acceptOrderController.js
 const db = require('../config/db');
-const { sendWhatsAppMessage } = require('../services/whatsappService');
 
 exports.acceptOrder = async (req, res) => {
     const { orderId, pharmacyId } = req.body;
@@ -24,29 +23,8 @@ exports.acceptOrder = async (req, res) => {
             return res.status(400).send('Erro ao aceitar o pedido.');
         }
 
-        // Obter informações do usuário para notificação
-        const [userData] = await db.execute(
-            `SELECT user_phone
-             FROM orders
-             WHERE id = ?`,
-            [orderId]
-        );
-
-        if (userData.length === 0) {
-            console.log('Erro: Não foi possível encontrar o número de telefone do usuário.');
-            return res.status(400).send('Erro ao buscar dados do usuário.');
-        }
-
-        const userPhone = userData[0].user_phone;
-
-        // Enviar a notificação para o usuário informando que o pedido foi aceito
-        const message = `Seu pedido foi aceito pela farmácia e será enviado em breve. Obrigado por escolher nosso serviço!`;
-
-        // Enviar a mensagem via WhatsApp
-        await sendWhatsAppMessage(phone_number_id, userPhone, message);
-
         // Responder com sucesso
-        return res.status(200).send('Pedido aceito e notificação enviada.');
+        return res.status(200).send('Pedido aceito.');
 
     } catch (error) {
         console.error('Erro ao aceitar pedido:', error);
