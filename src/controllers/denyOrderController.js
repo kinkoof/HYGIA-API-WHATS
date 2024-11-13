346b7f0d866adeb2cc0afb65ecbd2556d2c67068
lucas_ferrari <lucas.ferrari3007@gmail.com>
2024-11-13 04:02:02 -0300

    zcsd

--- src/controllers/denyOrderController.js
+++ src/controllers/denyOrderController.js
@@ -1,9 +1,14 @@
+// denyOrderController.js
 const db = require('../config/db');
-const { sendWhatsAppMessage } = require('../services/whatsappService');
 
 exports.denyOrder = async (phone_number_id, pharmacyId, orderId, res) => {
+    // Verifique se os parâmetros necessários foram passados
+    if (!orderId || !pharmacyId) {
+        return res.status(400).send('Faltam parâmetros necessários: orderId ou pharmacyId.');
+    }
+
     try {
-        // Atualizar o status da ordem para "x" (recusado pela farmácia)
+        // Atualizar o status da ordem para "r" (recusado pela farmácia)
         const [result] = await db.execute(
             `UPDATE orders
              SET status = 'x', pharmacy_id = ?
@@ -16,29 +21,8 @@ exports.denyOrder = async (phone_number_id, pharmacyId, orderId, res) => {
             return res.status(400).send('Erro ao recusar o pedido.');
         }
 
-        // Obter informações do usuário para notificação
-        const [userData] = await db.execute(
-            `SELECT user_phone
-             FROM orders
-             WHERE id = ?`,
-            [orderId]
-        );
-
-        if (userData.length === 0) {
-            console.log('Erro: Não foi possível encontrar o número de telefone do usuário.');
-            return res.status(400).send('Erro ao buscar dados do usuário.');
-        }
-
-        const userPhone = userData[0].user_phone;
-
-        // Enviar a notificação para o usuário informando que o pedido foi recusado
-        const message = `Infelizmente, seu pedido foi recusado pela farmácia. Sinta-se à vontade para tentar novamente ou contatar outra farmácia.`;
-
-        // Enviar a mensagem via WhatsApp
-        sendWhatsAppMessage(phone_number_id, userPhone, message, res);
-
         // Responder com sucesso
-        return res.status(200).send('Pedido recusado e notificação enviada.');
+        return res.status(200).send('Pedido recusado.');
 
     } catch (error) {
         console.error('Erro ao recusar pedido:', error);
